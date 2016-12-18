93006a8644860b694208d825a373eeff04fb08f3
Mikko Ronkainen <mikko@mikkoronkainen.com>
2016-12-18 14:07:20 +0200

    Updated external libraries

--- platform/windows/post-build.bat
+++ platform/windows/post-build.bat
@@ -4,13 +4,13 @@ if not exist bin\%1\data (
   xcopy /E /I /Y /Q data bin\%1\data
 )
 
-if not exist bin\%1\data\dll (
-  xcopy /E /I /Y /Q platform\windows\dll bin\%1\data\dll
-)
+:: if not exist bin\%1\data\dll (
+::  xcopy /E /I /Y /Q platform\windows\dll bin\%1\data\dll
+:: )
 
-if not exist bin\%1\varjo.exe.config (
-  xcopy /Y /Q platform\windows\varjo.exe.config bin\%1
-)
+:: if not exist bin\%1\varjo.exe.config (
+::  xcopy /Y /Q platform\windows\varjo.exe.config bin\%1
+:: )
 
 if not exist bin\%1\varjo.ini (
   xcopy /Y /Q misc\varjo.ini bin\%1
